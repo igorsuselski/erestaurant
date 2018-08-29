a984685d3c526eb00fc146aac8dcf5534c0b9a39
StefanD1997 <37450413+StefanD1997@users.noreply.github.com>
2018-08-29 21:11:48 +0200

    model classes

---
+++ WebAppERestaurant/SEDC.ERestaurant.DATA/Model/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.ERestaurant.Data.Model
+{
+    class Menu
+    {       
+        public int Id { get; set; }
+        public Type Type { get; set; }
+        public string RestaurantName { get; set; }
+        public List<Category> CategoryList { get; set; }
+    }
+
+    enum Type
+    {
+        Meals,
+        Drinks,
+        Wiens
+    }
+}
---
+++ WebAppERestaurant/SEDC.ERestaurant.DATA/Model/Orders.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.ERestaurant.Data.Model
+{
+    class Orders
+    {
+        public int Quantity { get; set; }
+        public int TotalQuantity { get; set; }
+        public int Table { get; set; }
+        public double TotalPrize { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public string Comments { get; set; }
+        public DateTime Created { get; set; }
+        public List<Items> ItemList { get; set; }
+    }
+
+    enum OrderStatus
+    {
+        created,
+        inProcess,
+        prepared,
+        unprepared
+    }
+}
